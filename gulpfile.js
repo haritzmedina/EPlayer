0e4d9db7072f051b5405aff4b68b8821e94df8f9
Haritz Medina <haritzmedina@gmail.com>
2017-02-15 18:21:20 +0100

    Searching is now enabled on library. Create a playlist from a certain library is allowed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
 // generated on 2016-05-24 using generator-chrome-extension 0.5.6
-var gulp = require('gulp');
-var gulpLoadPlugins = require('gulp-load-plugins');
-var del = require('del');
-var runSequence = require('run-sequence');
-var source = require('vinyl-source-stream');
-var browserify = require('browserify');
-var uglify = require('gulp-uglify');
-var pump = require('pump');
-var babel  = require("gulp-babel");
-var jsdoc = require('gulp-jsdoc3');
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const del = require('del');
+const runSequence = require('run-sequence');
+const source = require('vinyl-source-stream');
+const browserify = require('browserify');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+const babel  = require("gulp-babel");
+const jsdoc = require('gulp-jsdoc3');
+const electron = require('electron-connect').server.create();
+
 
 
 const $ = gulpLoadPlugins();
+
+
+gulp.task('electron', ()=>{
+  "use strict";
+  // Start browser process
+  electron.start();
+
+  // Reload renderer process
+  gulp.watch('app/scripts.babel/**/*.js', ['lint', 'browserify', electron.reload]);
+
+});
+
 
 gulp.task('extras', () => {
   return gulp.src([
@@ -32,7 +46,7 @@ gulp.task('extras', () => {
 });
 
 gulp.task('doc', function (cb) {
-  var config = require('./jsdoc.json');
+  let config = require('./jsdoc.json');
   gulp.src(['README.md', './app/scripts.babel/**/*.js'], {read: false})
     .pipe(jsdoc(config, cb));
 });
@@ -113,8 +127,12 @@ gulp.task('chromeManifest', () => {
 });
 
 gulp.task('browserify', function() {
-  return browserify('./app/scripts.babel/Window.js')
-    .bundle()
+  return browserify('./app/scripts.babel/Window.js', {
+    insertGlobalVars : {process: ()=>{}
+    },
+    builtins: false
+  })
+  .bundle()
     //Pass desired output filename to vinyl-source-stream
     .pipe(source('Window.js'))
     // Start piping stream to tasks!
@@ -142,7 +160,7 @@ gulp.task('size', () => {
 });
 
 gulp.task('package', function () {
-  var manifest = require('./dist/manifest.json');
+  var manifest = require('./app/manifest.json');
   return gulp.src('dist/**')
       .pipe($.zip('GCPlayer-' + manifest.version + '.zip'))
       .pipe(gulp.dest('package'));
